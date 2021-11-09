e8b9d6a0d28d666cfaa0d46cf5663153065fe95a
ManishVangara <86878532+ManishVangara@users.noreply.github.com>
2021-11-09 22:43:41 +0530

    Update 3. Guess the number (computer guess).py

--- 3. Guess the number (computer guess).py
+++ 3. Guess the number (computer guess).py
@@ -15,21 +15,5 @@ def computer_guess(x):
     print(f'Finally! The computer guessed your number, {guess}, correctly!')
 
 
-computer_guess(20000)
-
-
-# def computer_guess(x):
-#     low = 1 
-#     high =  x 
-#     feedback = ""
-#     while feedback != "C" :
-#         if low != high :
-#             guess = random.randint(low,high)
-#         else :
-#             guess = low         # could also be high b/c low = high
-#         feedback = input(f"Is {guess} too high (H), too low (L), or Correct (C)")
-#         if feedback == "H":
-#             high = guess - 1
-#         elif feedback == "L":
-#             low = guess + 1
-#     print(f'Yay! The computer guessed your number, {guess}, correctly!')
+x = int(input("Please enter a number of your choice: "))
+computer_guess(x)
